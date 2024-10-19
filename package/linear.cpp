a3db93be141fbc58206e4f7f1fe6666d73ffb0fd
Manish Suthar <manishsuthar078@gmail.com>
2024-10-19 19:16:31 +0530

    chore: linear helper added; palindrome added

---
+++ package/linear.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print_arr(auto *arr, int size) {
+  cout << endl;
+  for (int i; i < size; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
+
+void print_vect(auto *vect) {
+  for (auto i : vect) {
+    cout << i << " ";
+  }
+}
--- step_1/lec_5/palindrome_recursion.cpp
+++ step_1/lec_5/palindrome_recursion.cpp
@@ -1,20 +1,32 @@
 #include <bits/stdc++.h>
+
+#include "../../package/linear.cpp"
 using namespace std;
 
-void palindrome(int *arr, int s, int e) {
+void reverse(int *arr, int s, int e) {
   // base case
   if (s >= e) return;
   swap(arr[s], arr[e]);
-  palindrome(arr, s + 1, e - 1);
+  reverse(arr, s + 1, e - 1);
+}
+
+bool palindrome(int *arr, int s, int e) {
+  // base case
+  if (s >= e) return true;
+  if (arr[s] != arr[e]) {
+    return false;
+  } else {
+    palindrome(arr, s + 1, e - 1);
+  }
 }
 
 int main() {
-  int arr[] = {1, 2, 3, 4};
-  int s = 0, e = 3;
-  palindrome(arr, s, e);
-
-  for (auto i : arr) {
-    cout << i << " ";
-  }
+  int arr1[] = {1, 2, 3, 4};
+  int arr2[] = {1, 1, 1, 1, 2, 1, 1, 1, 1};
+  int s1 = 0, e1 = sizeof(arr1) / sizeof(*arr1);
+  int s2 = 0, e2 = sizeof(arr2) / sizeof(*arr2);
+  reverse(arr1, s1, e1 - 1);
+  print_arr(arr1, e1);
+  cout << endl << "Is Arr2 Palindrome: " << palindrome(arr2, s2, e2 - 1);
   return 0;
 }
