e51109783bafc36c872ba6a9f75c2c05870e3a2d
Manish Suthar <manishsuthar078@gmail.com>
2024-10-19 21:01:42 +0530

    chore: solved string_hashing and num_hashing

--- step_1/lec_6/hashing.cpp
+++ step_1/lec_6/hashing.cpp
@@ -3,7 +3,7 @@
 #include "../../package/linear.cpp"
 using namespace std;
 
-vector<int> num_of_occurance(vector<int> arr) {
+vector<int> num_of_occurance_in_int_arr(vector<int> arr) {
   int max = *max_element(arr.begin(), arr.end());
   vector<int> hash(max + 1, 0);
   // hash = {0,1,2,3,4,5,6,7,8,9,10}
@@ -13,9 +13,25 @@ vector<int> num_of_occurance(vector<int> arr) {
   return hash;
 }
 
+vector<int> num_of_occurance_in_string(string str) {
+  // initialize array of size 26 with all elements as 0
+  // since there are 26 alphabets in english
+  vector<int> hash(26, 0);
+  for (int i = 0; str[i] != '\0'; i++) {
+    int idx = str[i] - 'a';
+    ++hash[idx];
+  }
+  return hash;
+}
+
 int main() {
-  vector<int> arr = {1, 2, 1, 3, 4, 5, 4, 3, 2, 5, 2, 1, 1, 6, 7, 10};
-  vector<int> hash = num_of_occurance(arr);
-  print_vect(hash);
+  vector<int> int_arr = {1, 2, 1, 3, 4, 5, 4, 3, 2, 5, 2, 1, 1, 6, 7, 10};
+  string str = "bananaz";
+  vector<int> hash_int = num_of_occurance_in_int_arr(int_arr);
+  vector<int> hash_char = num_of_occurance_in_string(str);
+  cout << "Number of occurrene in Integer array: ";
+  print_vect(hash_int);
+  cout << "\nNumber of occurrene in String: ";
+  print_vect(hash_char);
   return 0;
 }
