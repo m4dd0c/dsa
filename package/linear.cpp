17f4fe758c1e5fc0914fc50ad084d927d2530974
Manish Suthar <manishsuthar078@gmail.com>
2024-10-21 02:00:10 +0530

    fix(linear.module): print_arr func

--- package/linear.cpp
+++ package/linear.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print_arr(auto* arr, int size) {
+void print_arr(auto *arr, int size) {
   cout << endl;
-  for (int i; i < size; i++) {
+  for (int i = 0; i < size; i++) {
     cout << arr[i] << " ";
   }
   cout << endl;
@@ -16,8 +16,8 @@ void print_vect(vector<int> vect) {
 }
 
 template <typename K, typename V>
-void print_map(const unordered_map<K, V>& mpp) {
-  for (const auto& pair : mpp) {
+void print_map(const unordered_map<K, V> &mpp) {
+  for (const auto &pair : mpp) {
     cout << pair.first << " -> " << pair.second << endl;
   }
 }
