24646ff02b9c06be28e5d2cd19c1433aafdfc40a
Manish Suthar <manishsuthar078@gmail.com>
2024-10-24 19:16:53 +0530

    perf: optimized, using flag

--- step_02/sorting_1/bubble_sort.cpp
+++ step_02/sorting_1/bubble_sort.cpp
@@ -4,10 +4,16 @@ using namespace std;
 
 void bubble(vector<int> &arr) {
   for (int i = 0; i < arr.size(); i++) {
+    int flag = 0;
     for (int j = 0; j < arr.size() - i; j++) {
-      if (arr[j] > arr[j + 1])
+      if (arr[j] > arr[j + 1]) {
         swap(arr[j], arr[j + 1]);
+        flag = 1;
+      }
     }
+    // meaning array is always sorted that It never went swapping in one go.
+    if (flag == 0)
+      break;
   }
 }
 
